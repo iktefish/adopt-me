4e47f6f46b8a6f50334b9b98d0874ec8981c5ded
iktefish <87042742+iktefish@users.noreply.github.com>
2021-12-12 23:30:14 +0600

    The `useState` hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,26 @@
+import { useState } from "react"; // This is a `hook
+
+/* Hooks allow us to keep track of states. All hooks begin with `use`. */
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+  // const location = "Seattle, WA";
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Leader" />
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            /* React, as the name says, is "Reactive" in nature. As such the order a series of reactions
+      take is very important is frail. Messing up order can easily render a series of hooks unpredictable.
+      Therefore never put hooks in `if` statements, `for` loops, etc. */
+            /* React also doesn't render the entire page upon every reaction. It is very intelligent in modularizing
+      a page and re-rendering only necessary segments. */
+            value={location}
+            placeholder="Leader"
+          />
         </label>
         <button>Submit</button>
       </form>
