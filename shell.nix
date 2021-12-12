f8086e0a65facc1d5bc6ccfb594a3d14e56779bd
iktefish <87042742+iktefish@users.noreply.github.com>
2021-12-12 16:48:23 +0600

    Installing and configuring dev tools

---
+++ shell.nix
@@ -0,0 +1,26 @@
+with import <nixpkgs> {};
+let nodeNix = import ~/Arcane/_n/node/default.nix {};
+    html-languageserver = pkgs.writeShellScriptBin ''vscode-html-language-server'' ''html-languageserver "$@"'';
+    css-languageserver = pkgs.writeShellScriptBin ''vscode-css-language-server'' ''css-languageserver "$@"'';
+    scripts = pkgs.writeShellScriptBin ''scripts'' ''jq ".scripts" package.json'';
+    run = pkgs.writeShellScriptBin ''run'' ''exec npm run "$@"'';
+
+in stdenv.mkDerivation {
+    name = "node";
+    buildInputs = [
+        jq
+        nodejs
+        nodeNix.typescript
+        nodeNix.typescript-language-server
+        nodeNix.vscode-html-languageserver-bin
+        nodeNix.vscode-css-languageserver-bin
+        nodeNix.diagnostic-languageserver
+        nodeNix.emmet-ls
+        nodeNix.prettier
+        nodeNix.eslint_d
+        html-languageserver
+        css-languageserver
+        scripts
+        run
+    ];
+}
